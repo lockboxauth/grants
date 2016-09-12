0616f4620f05586dfbca492ebf9c9cc85a2efce9
Paddy <paddy@impractical.co>
2016-09-11 23:30:15 -0700

    Add travis and build scripts.
    Add a travis configuration file to get automated testing going.
    
    Add a build script. This doesn’t do much at the moment, as we don’t have
    any `main` packages, but it’s good to have for when we do?

---
+++ build.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# set our ldflags to inject version info
+PACKAGE_PREFIX=code.impractical.co/grants
+source vendor/darlinggo.co/version/ldflags.sh
+
+# build the binary
+CGO_ENABLED=$CGO_ENABLED GOOS=$GOOS GOARCH=$GOARCH go build -o ./tokensd/tokensd -ldflags "${LDFLAGS}" ./grantsd
