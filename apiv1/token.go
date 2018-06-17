3a00a672a0321980d906ec3a0159d02119441f72
Paddy Carver <paddy@impractical.co>
2018-06-17 11:40:15 -0700

    Move API package to oauth2 package.
    There's no real reason the oauth2 logic should live in the API for the
    grants service, so let's pull it out into an impractical.co/auth/oauth2
    package.

--- apiv1/token.go
+++
@@ -1,9 +0,0 @@
-package apiv1
-
-type Token struct {
-	AccessToken  string `json:"access_token"`
-	TokenType    string `json:"token_type"`
-	ExpiresIn    int    `json:"expires_in,omitempty"`
-	RefreshToken string `json:"refresh_token,omitempty"`
-	Scope        string `json:"scope,omitempty"`
-}
