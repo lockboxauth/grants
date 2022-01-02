af4c7992cb89323bc7bbf1ff47ceef2660e89175
Paddy Carver <paddy@impractical.co>
2022-01-02 03:38:41 -0800

    Surface migrations source for postgres storer.

--- storers/postgres/migrations.go
+++ storers/postgres/migrations.go
@@ -13,10 +13,16 @@ var migrations embed.FS
 // ApplyMigrations runs the necessary database migrations to make the database
 // match the expected schema against the passed connection.
 func ApplyMigrations(connection *sql.DB, direction migrate.MigrationDirection) error {
-	migrations := &migrate.EmbedFileSystemMigrationSource{
+	migrations := MigrationsSource()
+	_, err := migrate.Exec(connection, "postgres", migrations, direction)
+	return err
+}
+
+// MigrationsSource returns a migrate.MigrationSource to apply the migrations
+// for this storer.
+func MigrationsSource() migrate.MigrationSource {
+	return &migrate.EmbedFileSystemMigrationSource{
 		FileSystem: migrations,
 		Root:       "sql",
 	}
-	_, err := migrate.Exec(connection, "postgres", migrations, direction)
-	return err
 }
